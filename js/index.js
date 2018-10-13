8aa0a653900355e1999d3155ae3b2948b4be2df2
annaitamil <tiruvarur.ramanan@gmail.com>
2018-10-13 19:22:46 +0530

    சித்தர் பாடல்கள் இணைக்கப்படுகிறது
    1. பத்திரகிரியார் மெய்ஞ்ஞான புலம்பல், கொங்கணவர் வாலை கும்மி இணைக்கப்படுகிறது
    2. சிவவாக்கியம், திருக்குறள், திருமந்திரம் புதுப்பிக்கப்பட்டது

--- js/index.js
+++ js/index.js
@@ -38,27 +38,34 @@ app.on({ page: 'thirukkural', content: 'thirukkural.html' }, function (activity)
         var json = null;
         var index = 0;
 
-        $("#forward").click(function () {
+        $("#kural-forward").click(function () {
             index = index + 1;
             index = (index > 1329) ? 0 : index;
             update();
         });
 
-        $("#back").click(function () {
+        $("#kural-back").click(function () {
             index = index - 1;
             index = (index < 0) ? 1329 : index;
             update();
         });
 
+        $("#kural").click(function () {
+            var prompt = phonon.prompt('குறள் எண்', '');
+            prompt.on('confirm', function (value) {
+                index = parseInt(value);
+                update();
+            });
+        });
+
         var update = function () {
             $('#kural').text(index + 1);
-
-            $('#kural-line1').text(json.content[index].line1);
-            $('#kural-line2').text(json.content[index].line2);
-            $('#chapter').text(json.content[index].chapter);
-            $('#group').text(json.content[index].group);
-            $('#section').text(json.content[index].section);
-            $('#explanation').text(json.content[index].explanation);
+            $('#kural-line1').text(json.content[index][json.abbreviations['line1']]);
+            $('#kural-line2').text(json.content[index][json.abbreviations['line2']]);
+            $('#kural-chapter').text(json.chapters[json.content[index][json.abbreviations['chapter']]]);
+            $('#kural-group').text(json.groups[json.content[index][json.abbreviations['group']]]);
+            $('#kural-section').text(json.sections[json.content[index][json.abbreviations['section']]]);
+            $('#kural-explanation').text(json.content[index][json.abbreviations['explanation']]);
         };
 
         load("json/thirukkural.json", function (content) {
@@ -78,7 +85,7 @@ app.on({ page: 'tamilagaradhi', content: 'tamilagaradhi.html' }, function (activ
             Object.keys(json.content).forEach(function (key) {
                 listItems += '<li><div class="item-content"><span class="title list-title">' + key + '</span><span class="body list-text">' + json.content[key] + '</span></div></li>';
             });
-            
+
             $('#tamilagaradhi-list').html(listItems);
         };
 
@@ -94,13 +101,13 @@ app.on({ page: 'sivavakkiyam', content: 'sivavakkiyam.html' }, function (activit
         var json = null;
         var index = 0;
 
-        $("#forward").click(function () {
+        $("#sivavakkiyam-forward").click(function () {
             index = index + 1;
             index = (index > 524) ? 0 : index;
             update();
         });
 
-        $("#back").click(function () {
+        $("#sivavakkiyam-back").click(function () {
             index = index - 1;
             index = (index < 0) ? 524 : index;
             update();
@@ -108,15 +115,79 @@ app.on({ page: 'sivavakkiyam', content: 'sivavakkiyam.html' }, function (activit
 
         var update = function () {
             $('#sivavakkiyam').text(index + 1);
-
-            $('#group').text(json.content[index].group);
-            $('#line1').text(json.content[index].line1);
-            $('#line2').text(json.content[index].line2);
-            $('#line3').text(json.content[index].line3);
-            $('#line4').text(json.content[index].line4);
+            $('#sivavakkiyam-group').text(json.groups[json.content[index][json.abbreviations['group']]]);
+            $('#sivavakkiyam-line1').text(json.content[index][json.abbreviations['line1']]);
+            $('#sivavakkiyam-line2').text(json.content[index][json.abbreviations['line2']]);
+            $('#sivavakkiyam-line3').text(json.content[index][json.abbreviations['line3']]);
+            $('#sivavakkiyam-line4').text(json.content[index][json.abbreviations['line4']]);
         };
 
         load("json/sivavakkiyam.json", function (content) {
+            json = content;
+            index = 0;
+            update();
+        });
+    });
+});
+
+app.on({ page: 'konganar_vaalai_kummi', content: 'konganar_vaalai_kummi.html' }, function (activity) {
+    activity.onReady(function () {
+        var json = null;
+        var index = 0;
+
+        $("#konganar_vaalai_kummi-forward").click(function () {
+            index = index + 1;
+            index = (index > 110) ? 0 : index;
+            update();
+        });
+
+        $("#konganar_vaalai_kummi-back").click(function () {
+            index = index - 1;
+            index = (index < 0) ? 110 : index;
+            update();
+        });
+
+        var update = function () {
+            $('#konganar_vaalai_kummi').text(index + 1);
+            $('#konganar_vaalai_kummi-section').text(json.sections[json.content[index][json.abbreviations['section']]]);
+            $('#konganar_vaalai_kummi-line1').text(json.content[index][json.abbreviations['line1']]);
+            $('#konganar_vaalai_kummi-line2').text(json.content[index][json.abbreviations['line2']]);
+            $('#konganar_vaalai_kummi-line3').text(json.content[index][json.abbreviations['line3']]);
+            $('#konganar_vaalai_kummi-line4').text(json.content[index][json.abbreviations['line4']]);
+        };
+
+        load("json/konganar_vaalai_kummi.json", function (content) {
+            json = content;
+            index = 0;
+            update();
+        });
+    });
+});
+
+app.on({ page: 'badragiriyar_meignana_pulambal', content: 'badragiriyar_meignana_pulambal.html' }, function (activity) {
+    activity.onReady(function () {
+        var json = null;
+        var index = 0;
+
+        $("#badragiriyar_meignana_pulambal-forward").click(function () {
+            index = index + 1;
+            index = (index > 233) ? 0 : index;
+            update();
+        });
+
+        $("#badragiriyar_meignana_pulambal-back").click(function () {
+            index = index - 1;
+            index = (index < 0) ? 233 : index;
+            update();
+        });
+
+        var update = function () {
+            $('#badragiriyar_meignana_pulambal').text(index + 1);
+            $('#badragiriyar_meignana_pulambal-line1').text(json.content[index][json.abbreviations['line1']]);
+            $('#badragiriyar_meignana_pulambal-line2').text(json.content[index][json.abbreviations['line2']]);
+        };
+
+        load("json/badragiriyar_meignana_pulambal.json", function (content) {
             json = content;
             index = 0;
             update();
@@ -129,13 +200,13 @@ app.on({ page: 'thirumanthiram', content: 'thirumanthiram.html' }, function (act
         var json = null;
         var index = 0;
 
-        $("#forward").click(function () {
+        $("#thirumanthiram-forward").click(function () {
             index = index + 1;
             index = (index > 3001) ? 0 : index;
             update();
         });
 
-        $("#back").click(function () {
+        $("#thirumanthiram-back").click(function () {
             index = index - 1;
             index = (index < 0) ? 3001 : index;
             update();
@@ -143,13 +214,12 @@ app.on({ page: 'thirumanthiram', content: 'thirumanthiram.html' }, function (act
 
         var update = function () {
             $('#thirumanthiram').text(index + 1);
-
-            $('#group').text(json.content[index].group);
-            $('#chapter').text(json.content[index].chapter);
-            $('#line1').text(json.content[index].line1);
-            $('#line2').text(json.content[index].line2);
-            $('#line3').text(json.content[index].line3);
-            $('#line4').text(json.content[index].line4);
+            $('#thirumanthiram-group').text(json.groups[json.content[index][json.abbreviations['group']]]);
+            $('#thirumanthiram-chapter').text(json.chapters[json.content[index][json.abbreviations['chapter']]]);
+            $('#thirumanthiram-line1').text(json.content[index][json.abbreviations['line1']]);
+            $('#thirumanthiram-line2').text(json.content[index][json.abbreviations['line2']]);
+            $('#thirumanthiram-line3').text(json.content[index][json.abbreviations['line3']]);
+            $('#thirumanthiram-line4').text(json.content[index][json.abbreviations['line4']]);
         };
 
         load("json/thirumanthiram.json", function (content) {
